b411a87a59ff9cd5802ba7f39dc76df812cffa4a
Daniel Kessler <sinani201@gmail.com>
2012-05-25 10:20:40 -0700

    experimental GUI code

---
+++ gui.hs
@@ -0,0 +1,27 @@
+module Main where
+
+import Graphics.UI.WX
+
+main :: IO ()
+main
+  = start gui
+
+gui :: IO ()
+gui
+  = do f <- frame [text := "Mafia Toolkit"]
+       leftLayout <- panel f []
+       sMafiaNum <- spinCtrl leftLayout 1 99 []
+       sMaxPlayers <- spinCtrl leftLayout 1 99 []
+       sMafiaKP <- spinCtrl leftLayout 0 99 []
+       cDayNight <- comboBox leftLayout [items := ["Day","Night"]]
+
+       sCycle <- spinCtrl leftLayout 0 99 []
+
+       set f [layout := container leftLayout $
+                        margin 10 $
+                        column 5 [boxed "Game Setup"
+                        (grid 5 5 [[label "Mafia #",widget sMafiaNum]
+                                  ,[label "Max Players",widget sMaxPlayers]
+                                  ,[label "Mafia KP",widget sMafiaKP]
+                                  ,[widget cDayNight,widget sCycle]])]]
+       return ()
