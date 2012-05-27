ff66919c1a115e2c21e7f02438b3f70f9476b154
Daniel Kessler <sinani201@gmail.com>
2012-05-26 22:15:08 -0700

    the spinners now have a minimum size

--- gui.hs
+++ gui.hs
@@ -6,6 +6,9 @@ main :: IO ()
 main
   = start gui
 
+spinnerW :: Int
+spinnerW = 40
+
 gui :: IO ()
 gui
   = do f <- frame [text := "Mafia Toolkit"]
@@ -13,15 +16,17 @@ gui
        sMafiaNum <- spinCtrl leftLayout 1 99 []
        sMaxPlayers <- spinCtrl leftLayout 1 99 []
        sMafiaKP <- spinCtrl leftLayout 0 99 []
-       cDayNight <- choice leftLayout [items := ["Day","Night"]]
 
+       defaultSize <- get sMafiaKP size 
+       
+       cDayNight <- choice leftLayout [items := ["Day","Night"] ]
        sCycle <- spinCtrl leftLayout 0 99 []
 
        set f [layout := container leftLayout $
                         margin 10 $
                         column 5 [boxed "Game Setup"
-                        (grid 5 5 [[label "Mafia #",widget sMafiaNum]
-                                  ,[label "Max Players",widget sMaxPlayers]
-                                  ,[label "Mafia KP",widget sMafiaKP]
-                                  ,[widget cDayNight,widget sCycle]])]]
+                        (grid 5 5 [[label "Mafia #",minsize (defaultSize {sizeW = spinnerW}) (widget sMafiaNum)]
+                                  ,[label "Max Players",minsize (defaultSize {sizeW = spinnerW}) (widget sMaxPlayers)]
+                                  ,[label "Mafia KP",minsize (defaultSize {sizeW = spinnerW}) (widget sMafiaKP)]
+                                  ,[widget cDayNight,minsize (defaultSize {sizeW = spinnerW}) (widget sCycle)]])]]
        return ()
