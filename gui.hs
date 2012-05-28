30c426aa2294f0b362422978284ed5baa2a99216
Daniel Kessler <sinani201@gmail.com>
2012-05-27 21:33:54 -0700

    properly placed the tab window, and added a potential grid for the role setup page

--- gui.hs
+++ gui.hs
@@ -30,20 +30,19 @@ gui
 
        -- Role Setup page
        p1 <- panel nb []
-       -- stuff goes here
        
        -- Simulation page
        p2 <- panel nb []
 
        set f [layout := container p $
                         margin 10 $
-                        column 5 [boxed "Game Setup"
+                        row 5 [boxed "Game Setup"
                         (grid 5 5 [[label "Mafia #",minsize (defaultSize {sizeW = spinnerW}) (widget sMafiaNum)]
                                   ,[label "Max Players",minsize (defaultSize {sizeW = spinnerW}) (widget sMaxPlayers)]
                                   ,[label "Mafia KP",minsize (defaultSize {sizeW = spinnerW}) (widget sMafiaKP)]
                                   ,[widget cDayNight,minsize (defaultSize {sizeW = spinnerW}) (widget sCycle)]])
-                        , glue
-                        ,row 5 [tabs nb
-                          [tab "Role Setup" $ container p1 $ margin 10 $ column 5 [label "page 1"]
+                        ,floatTopRight $ row 5 [tabs nb
+                          [tab "Role Setup" $ container p1 $ margin 10 $ column 5 [grid 5 5
+                            [[label "Vanilla",]]]
                           ,tab "Simulation" $ container p2 $ margin 10 $ column 5 [label "page 2"]]]]]
        return ()
