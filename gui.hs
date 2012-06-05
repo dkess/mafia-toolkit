2b6767a1d933d2417597c9ee291819ce5fbe504d
Daniel Kessler <sinani201@gmail.com>
2012-06-05 14:21:01 -0700

    the show values for mafiarole now just use the first letter, for extra readability. also fixed a fill bug

--- gui.hs
+++ gui.hs
@@ -68,7 +68,7 @@ gui
               mafiaNum <- get sMafiaNum selection
               maxPlayers <- get sMaxPlayers selection
 
-              print $ [i ++ (maybe [] (\y -> replicate (maxPlayers - mafiaNum - length i) (fst y))
+              print $ [i ++ (maybe [] (\y -> replicate (maxPlayers - mafiaNum - length [z | z <- i, MafiaRole.color z /= Red]) (fst y))
                          (listToMaybe [x | x <- listFromBools (zip roleList minVal) (zipWith (&&) fillVals enabledVals)
                                          , (MafiaRole.color (fst x)) == Green]))
                          ++ (maybe [] (\y -> replicate (mafiaNum - length [x | x <- i, MafiaRole.color x == Red]) (fst y))
